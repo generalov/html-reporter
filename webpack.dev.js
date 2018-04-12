bc4a3c67411e9fb4689a1d54676809ab402bea01
Evgeniy Generalov <evgeniy_generalov@epam.com>
2018-04-12 18:39:19 +0300

    fix: fix

--- webpack.dev.js
+++ webpack.dev.js
@@ -12,7 +12,16 @@ module.exports = merge(
         devServer: {
             contentBase: './lib/static',
             inline: true,
-            hot: true
+            hot: true,
+            proxy: {
+                '/events': 'http://localhost:8000',
+                '/images': 'http://localhost:8000',
+                '/init': 'http://localhost:8000',
+                '/load': 'http://localhost:8000',
+                '/run': 'http://localhost:8000',
+                '/save': 'http://localhost:8000',
+                '/update-reference': 'http://localhost:8000'
+            }
         },
         plugins: [
             new webpack.HotModuleReplacementPlugin()
